046f980d0f662233c4b2b7c0c2f9725302b006c2
Tyler Spadgenske <tyspadgens@gmail.com>
2016-12-20 12:04:35 -0600

    added light sensors

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -8,10 +8,14 @@
 #define BATTERYSENSOR 0
 #define STAMPSSENSORA 4
 #define STAMPSSENSORB 2
+#define POSTITSENSOR A0
+#define PAPERCLIPSENSOR A1
 
 //initialize variables
 int stampSensorCount = 0;
 int stampCount = 100;
+int PAPERCLIP_THRESHOLD = 3400;
+int POSTIT_THRESHOLD = 350;
 
 void setup() {
     // initialize the LED pin as an output:
@@ -23,6 +27,10 @@ void setup() {
     digitalWrite(STAMPSSENSORA, HIGH);
     pinMode(STAMPSSENSORB, INPUT);
     digitalWrite(STAMPSSENSORB, HIGH);
+    pinMode(POSTITSENSOR, INPUT);
+    digitalWrite(POSTITSENSOR, HIGH);
+    pinMode(PAPERCLIPSENSOR, INPUT);
+    digitalWrite(PAPERCLIPSENSOR, HIGH);
 
     Serial.begin();
 }
@@ -31,7 +39,7 @@ void loop(){
     //poll sensors
     //check batteries
     if (digitalRead(BATTERYSENSOR) == HIGH) {
-        Serial.println("Low on batteries");
+        Serial.println("Out of batteries");
     }
     //check stamps
     if (digitalRead(STAMPSSENSORA) == 0 and digitalRead(STAMPSSENSORB) == 0) {
@@ -45,6 +53,15 @@ void loop(){
     }
     //Check total stamp count
     if (stampCount <= 0) {
-        Serial.println("Out of stamps");
+        Serial.println("Low on stamps");
     }
+    //check paperclips
+    if (analogRead(PAPERCLIPSENSOR) < PAPERCLIP_THRESHOLD) {
+        Serial.println("Low on paper clips");
+    }
+    //check postit! notes
+    if (analogRead(POSTITSENSOR) > POSTIT_THRESHOLD) {
+        Serial.println("Low on postit! notes");
+    }
+
 }
